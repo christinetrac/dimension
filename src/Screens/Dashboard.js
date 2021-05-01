fb24fc8b5ddc4f194e62504f457b9a36e9e7933f
christinetrac <christineetrac@gmail.com>
2021-05-01 14:54:26 -0400

    dashboard banner

--- src/Screens/Dashboard.js
+++ src/Screens/Dashboard.js
@@ -2,6 +2,8 @@ import React from 'react';
 import styles from './dashboard.module.css';
 import { NavBar } from "../Components/NavBar";
 import { Header } from "../Components/Header";
+import banner from "../assets/dashboard-banner.svg";
+import { MdLiveTv } from "react-icons/md";
 
 const Dashboard = (props) => {
     return (
@@ -9,7 +11,15 @@ const Dashboard = (props) => {
             <NavBar active={props.location.pathname}/>
             <Header/>
             <div className={styles.dashboard}>
+                <img src={banner} alt="" className={styles.banner}/>
+                <div className={styles.bannerText}><span>dimension</span> brings courses to <span>life.</span></div>
 
+                <div style={{marginLeft: '54px', marginTop:'-120px'}}>
+                    <div className={styles.title}>
+                        <MdLiveTv className={styles.titleIcon} style={{paddingTop: '6px'}}/>
+                        <div className={styles.titleText}>Live Courses</div>
+                    </div>
+                </div>
             </div>
         </div>
     );
