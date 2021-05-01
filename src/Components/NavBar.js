1c1656d83b444666d15e2e98fdaf04e4c3975f3f
christinetrac <christineetrac@gmail.com>
2021-05-01 01:33:45 -0400

    navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -5,6 +5,8 @@ import blob1 from '../assets/navbar-1.svg';
 import blob2 from '../assets/navbar-2.svg';
 import { MdDashboard } from "react-icons/md";
 import { MdSchool } from "react-icons/md";
+import { CgPlayListSearch } from "react-icons/cg";
+import { FaCube } from "react-icons/fa";
 
 const NavBar = (props) => {
     return (
@@ -13,22 +15,34 @@ const NavBar = (props) => {
             <img src={blob2} className={styles.blob2} alt="blob2"/>
             <div className={styles.navTopContainer}>
                 <Link to="/" className={styles.topLink}>
-                    <MdDashboard className={styles.topIcon}/>
-                    <p>
+                    <MdDashboard className={styles.topIcon} style={{color: props.active === '/' ? '#7544FF' : '#363636'}}/>
+                    <p style={{color: props.active === '/' ? '#7544FF' : '#363636'}}>
                         Dashboard
                     </p>
                 </Link>
                 <Link to="/your-courses" className={styles.topLink}>
-                    <MdSchool className={styles.topIcon}/>
-                    <p>
+                    <MdSchool className={styles.topIcon} style={{color: props.active === '/your-courses' ? '#7544FF' : '#363636'}}/>
+                    <p style={{color: props.active === '/your-courses' ? '#7544FF' : '#363636'}}>
                         Your Courses
                     </p>
                 </Link>
                 <Link to="/chat" className={styles.topLink}>
-                    <p>
+                    <CgPlayListSearch className={styles.topIcon} style={{color: props.active === '/chat' ? '#7544FF' : '#363636', width:'28px', height:'28px'}}/>
+                    <p style={{color: props.active === '/chat' ? '#7544FF' : '#363636'}}>
                         Chatty Chatter
                     </p>
                 </Link>
+            </div>
+            <div className={styles.navBottomContainer}>
+                <div className={styles.bottomLink}>
+                    <FaCube className={styles.dimensionIcon}/>
+                    <p>
+                        Your Dimensions
+                    </p>
+                </div>
+                <p className={styles.dimensionLinks}>Pottery</p>
+                <p className={styles.dimensionLinks}>Fitness</p>
+                <p className={styles.dimensionLinks}>Origami</p>
             </div>
         </div>
     );
